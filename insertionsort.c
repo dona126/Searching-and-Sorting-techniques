7e31302c48d23bbf5562d7443c563549d612c434
dona_maria_sunny <47379075+dona126@users.noreply.github.com>
2019-11-13 22:23:09 +0530

    Add files via upload

---
+++ insertionsort.c
@@ -0,0 +1,42 @@
+//INSERTION SORT
+
+
+#include<stdio.h>
+
+void insertionSort(int n,int a[]);
+void main()
+	{
+
+		int i,j,n,temp,a[25];
+		printf("Enter the number of elements to be sorted:");
+		scanf("%d",&n);
+		printf("Enter elements:");
+		for(i=1;i<n;i++)
+			{
+				scanf("%d",&a[i]);
+			}
+		insertionSort(n,a);
+	}
+void insertionSort(int n,int a[])
+	{
+		int i,temp,j;
+		for(i=1;i<n;i++)
+			{
+
+				temp=a[i];
+				j=i-1;
+				while(temp<a[j] && j>=0)
+					{
+						a[j+1]=a[j];
+						j=j-1;
+
+					}
+				a[j+1]=temp;
+			}
+		printf("Result is:\n");
+		for(i=1;i<n;i++)
+			{
+				printf("%d\n",a[i]);
+			}
+	}
+
